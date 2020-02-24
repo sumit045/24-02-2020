11c76e3e017e5aa347709b422a5ba44f80f96b2c
sumit045 <sumit619.hellboy@gmail.com>
2020-02-24 20:02:57 +0530

    Initial commit

--- TestGIT/src/test.java
+++ TestGIT/src/test.java
@@ -5,6 +5,14 @@ public class test {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello world");
+		
+		test t = new test();
+		t.testMethod();
+	}
+	
+	public void testMethod()
+	{
+		System.out.println("Hello again");
 	}
 
 }
